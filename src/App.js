4968ac08201de82198876ed8d584d0e17b55b331
Wejden09 <yakoubiwijden@gmail.com>
2025-08-11 11:52:37 +0100

    React_checkpoint

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './Navbar';
+import Heading from './Heading';
+import Heading2 from './Heading2';
+// row and col from react-bootstrap
+import Footer from './Footer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <Navbar />
+   <Heading />
+   <Footer />
+  
+    </>
+
   );
 }
 
